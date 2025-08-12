0f606b19f043e7503339fd25b4c302c16ee4610a
Heblemosdecaballos <carlos@tml.com.co>
2025-08-11 20:46:24 -0500

    Update NewThreadDialog.tsx

--- components/NewThreadDialog.tsx
+++ components/NewThreadDialog.tsx
@@ -1,4 +1,5 @@
-'use client'
+import { supabase } from '@/lib/supabaseClient'
+
 import { useState } from 'react'
 import { supabase } from '@/lib/supabaseClient'
 import { CATEGORIES } from '@/lib/utils'
