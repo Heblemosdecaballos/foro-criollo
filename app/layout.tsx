e698624ada6bde361a9515bdc800766d0938e414
Heblemosdecaballos <carlos@tml.com.co>
2025-08-14 10:27:41 -0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,21 +1,17 @@
-// app/layout.tsx (SERVER COMPONENT)
-import './globals.css';
+import { Cinzel, Montserrat } from 'next/font/google'
+
+const cinzel = Cinzel({ subsets: ['latin'], weight: ['400','700'] })
+const montserrat = Montserrat({ subsets: ['latin'], weight: ['300','400','500','700'] })
 
 export const metadata = {
   title: 'Hablemos de Caballos',
-  description: 'Foros de Hablemos de Caballos',
-};
+  description: 'Foro del Caballo Criollo Colombiano'
+}
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }) {
   return (
-    <html lang="es">
-      <body className="bg-white text-black antialiased">
-        {children}
-      </body>
+    <html lang="es" className={`${montserrat.className}`}>
+      <body className="bg-beige text-marron">{children}</body>
     </html>
-  );
+  )
 }
