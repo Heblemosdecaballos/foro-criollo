39f1a5d7850e4200423b3c24219dd6c7ca8e0883
Heblemosdecaballos <carlos@tml.com.co>
2025-08-15 17:39:18 -0500

    Update client.ts

--- app/utils/supabase/client.ts
+++ app/utils/supabase/client.ts
@@ -1,8 +1,5 @@
-// /utils/supabase/client.ts
 "use client";
-
 import { createBrowserClient } from "@supabase/ssr";
-
 export function createClient() {
   const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
   const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
