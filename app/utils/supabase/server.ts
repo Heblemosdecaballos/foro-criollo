98ec42b1fa6bb71350fc20bf541a5a58aa8f1001
Heblemosdecaballos <carlos@tml.com.co>
2025-08-15 15:48:33 -0500

    Create server.ts

---
+++ app/utils/supabase/server.ts
@@ -0,0 +1,27 @@
+// /utils/supabase/server.ts
+import { cookies } from "next/headers";
+import { createServerClient } from "@supabase/ssr";
+
+export function createSupabaseServer() {
+  const cookieStore = cookies();
+  const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+  const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+
+  return createServerClient(supabaseUrl, supabaseAnonKey, {
+    cookies: {
+      get(name: string) {
+        return cookieStore.get(name)?.value;
+      },
+      set(name: string, value: string, options: any) {
+        try {
+          cookieStore.set({ name, value, ...options });
+        } catch {}
+      },
+      remove(name: string, options: any) {
+        try {
+          cookieStore.set({ name, value: "", ...options });
+        } catch {}
+      },
+    },
+  });
+}
