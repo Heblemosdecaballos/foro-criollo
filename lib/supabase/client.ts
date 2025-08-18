dfc6a2ac8057f58331effef4458c5f1996859413
Heblemosdecaballos <carlos@tml.com.co>
2025-08-18 18:09:30 -0500

    Update client.ts

--- lib/supabase/client.ts
+++ lib/supabase/client.ts
@@ -5,4 +5,3 @@ export const supabase = createBrowserClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 );
-// Nota: sin adapter de cookies aquí (así no peleas con las httpOnly del servidor)
