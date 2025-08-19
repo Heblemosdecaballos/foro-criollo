68f66a29600cf5244daa3124968b258fa9b08136
Heblemosdecaballos <carlos@tml.com.co>
2025-08-19 11:57:41 -0500

    Update http.ts

--- utils/http.ts
+++ utils/http.ts
@@ -1,10 +1,5 @@
 // utils/http.ts
 import { getPublicBaseUrl } from './base-url'
-
-/**
- * apiFetch: construye la URL correctamente en CSR y SSR.
- * Úsalo así: apiFetch('/api/lo-que-sea')
- */
 export async function apiFetch(input: string, init?: RequestInit) {
   const isAbsolute = /^https?:\/\//i.test(input)
   const url = isAbsolute ? input : `${getPublicBaseUrl()}${input}`
