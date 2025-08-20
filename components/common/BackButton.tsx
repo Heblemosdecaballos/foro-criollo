ba357b71ba8e6e22aa74fd5da4343b5af406fb65
Heblemosdecaballos <carlos@tml.com.co>
2025-08-19 20:52:09 -0500

    Create BackButton.tsx

---
+++ components/common/BackButton.tsx
@@ -0,0 +1,13 @@
+// components/common/BackButton.tsx
+'use client'
+
+import { useRouter } from 'next/navigation'
+
+export default function BackButton({ className = 'btn btn-ghost' }: { className?: string }) {
+  const router = useRouter()
+  return (
+    <button type="button" className={className} onClick={() => router.back()}>
+      ← Atrás
+    </button>
+  )
+}
