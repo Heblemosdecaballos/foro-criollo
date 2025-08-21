869452388e9d1d77e7fabb5d6b4045b7962ae047
Heblemosdecaballos <carlos@tml.com.co>
2025-08-21 15:33:08 -0500

    Create client.ts

---
+++ src/lib/supabase/client.ts
@@ -0,0 +1,8 @@
+// /src/lib/supabase/client.ts
+export {
+  createSupabaseBrowserClient,
+  createSupabaseClient,
+  createSupabaseBrowser,
+  createSupabaseBrowserClientReadOnly,
+  createSupabaseBrowserReadOnly,
+} from "@/utils/supabase/client";
