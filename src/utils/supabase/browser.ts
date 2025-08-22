f6ab4e80922d413c6c9e809e470769ddca5021d7
Heblemosdecaballos <carlos@tml.com.co>
2025-08-22 18:50:50 -0500

    Create browser.ts

---
+++ src/utils/supabase/browser.ts
@@ -0,0 +1,5 @@
+// /src/utils/supabase/browser.ts
+"use client";
+
+// Reutiliza el cliente del navegador que definimos en src/lib/supabaseClient.ts
+export { supabase as supabaseBrowser } from "@/lib/supabaseClient";
