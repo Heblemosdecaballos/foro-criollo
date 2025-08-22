f4c15d4f835e8bc3fd608f71fd4f81dd39d96421
Heblemosdecaballos <carlos@tml.com.co>
2025-08-22 18:19:43 -0500

    Update supabaseClient.ts

--- src/lib/lib/supabaseClient.ts
+++ src/lib/lib/supabaseClient.ts
@@ -1,14 +1,10 @@
-'use client';
+// /src/lib/supabaseClient.ts
+"use client";
 
-import { createClient } from '@supabase/supabase-js';
+import { createBrowserClient } from "@supabase/ssr";
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
-  auth: {
-    persistSession: true,
-    autoRefreshToken: true,
-    detectSessionInUrl: true,
-  },
-});
+// Cliente de Supabase para componentes del navegador
+export const supabase = createBrowserClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+);
