f014eff468ed473b8538e75c0f2217e38841810e
Heblemosdecaballos <carlos@tml.com.co>
2025-08-21 20:46:11 -0500

    Create index.ts

---
+++ lib/supabase/client/index.ts
@@ -0,0 +1,9 @@
+// /lib/supabase/client/index.ts
+export { default } from "../client";
+export {
+  createSupabaseBrowser,
+  createSupabaseBrowserClient,
+  createSupabaseClient,
+  createSupabaseBrowserClientReadOnly,
+  createSupabaseBrowserReadOnly,
+} from "../client";
