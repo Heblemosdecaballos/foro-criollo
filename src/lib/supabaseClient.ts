9d1def0d71713ecee90ced0427fa2c68e08a5c96
Heblemosdecaballos <carlos@tml.com.co>
2025-08-22 19:16:55 -0500

    Update supabaseClient.ts

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -3,7 +3,6 @@
 
 import { createBrowserClient } from "@supabase/ssr";
 
-// Cliente de Supabase para componentes del navegador
 export const supabase = createBrowserClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
