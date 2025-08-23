4daf9f814a1acd1db80304ce998ebf0f9ce3c0c5
Heblemosdecaballos <carlos@tml.com.co>
2025-08-22 19:30:18 -0500

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,25 +1,10 @@
-// tailwind.config.ts
-import type { Config } from 'tailwindcss'
+// /tailwind.config.ts
+import type { Config } from "tailwindcss";
 
-export default {
-  content: [
-    './app/**/*.{ts,tsx,js,jsx,mdx}',
-    './components/**/*.{ts,tsx,js,jsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      fontFamily: {
-        sans: ['var(--font-sans)', 'system-ui', 'sans-serif'],
-        serif: ['var(--font-serif)', 'serif'],
-      },
-      colors: {
-        bg: 'var(--bg)',
-        fg: 'var(--fg)',
-      },
-      boxShadow: {
-        card: '0 12px 36px rgba(0,0,0,.08)'
-      }
-    },
-  },
+const config: Config = {
+  content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
+  theme: { extend: {} },
   plugins: [],
-} satisfies Config
+};
+
+export default config;
