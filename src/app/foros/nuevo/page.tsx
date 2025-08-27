cff0f8fcd263087de96cfa7c829e8f5fc3613396
Heblemosdecaballos <carlos@tml.com.co>
2025-08-27 08:16:13 -0500

    Update page.tsx

--- src/app/foros/nuevo/page.tsx
+++ src/app/foros/nuevo/page.tsx
@@ -9,7 +9,6 @@ export default async function NuevoForoPage() {
   return (
     <div className="max-w-2xl mx-auto p-6">
       <h1 className="text-2xl font-semibold mb-4">Crear nuevo foro / hilo</h1>
-
       {user ? (
         <NewThreadForm />
       ) : (
